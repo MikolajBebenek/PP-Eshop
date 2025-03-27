a20ba831bad157e78f08631a9fd31d5613762b59
MikolajBebenek <bebenek.mikolaj.official@gmail.com>
2025-03-27 16:37:13 +0100

    Initial commit

---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace EShopService.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace EShopService.Models
+{
+    public class Product : BaseModel
+    {
+        public string Name { get; set; }
+        public string Ean { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; } = 0;
+        public string Sku { get; set; }
+        public Category Category { get; set; }
+
+    }
+
+}
